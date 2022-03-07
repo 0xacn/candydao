05a6d145d24f72f0b6b6862a005a074b824ee941
Andrew <andrewnijmeh1@gmail.com>
2022-03-07 16:20:50 -0500

    🌱 (Make progress on initial sdk): Make progress on sdk script
    sdk script progress, check for priv key and alchemy api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import React from "react";
+import { useMemo, useState } from "react";
+import { useEffect } from "react";
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address:", address)
+  
+  if (!address) {
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Welcome to CandyDAO</h1>
+      <button onClick={() => connectWallet("injected")} classname="btn">
+        Connect your wallet
+      </button>
     </div>
   );
+ }
+
+  return (
+    <div className="landing">
+      <h1>wallet connected!</h1>
+    </div>);
 };
 
 export default App;
