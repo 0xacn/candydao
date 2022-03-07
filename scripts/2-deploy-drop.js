c57197226cd023ac607ed0b7242c70df43c09cea
Andrew <andrewnijmeh1@gmail.com>
2022-03-07 16:29:48 -0500

    add our image

--- scripts/2-deploy-drop.js
+++ scripts/2-deploy-drop.js
@@ -3,3 +3,13 @@ import { readFileSync } from "fs";
 import sdk from "./1-initialize-sdk.js";
 
 const app = sdk.getAppModule("INSERT_YOUR_APP_ADDRESS_HERE")
+
+(async () => {
+    try {
+        const bundleDropModule = await app.deployBundleDropModule({
+            name: "CandyDAO Membership",
+            description: "A DAO for candy lovers!",
+            image: readFileSync("scripts/assets/candy.png")
+        });
+    });
+})
